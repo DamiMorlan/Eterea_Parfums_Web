f6953b672b9731d1c0afae950613daa610ebebd6
AdrianBCampos <adrian.campos@davinci.edu.ar>
2025-06-23 19:05:48 -0300

    Haciendo html Carrito

---
+++ Eterea_Parfums_Web/ViewModels/ItemCarritoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Eterea_Parfums_Web.ViewModels
+{
+    public class ItemCarritoViewModel
+    {
+        public int PerfumeId { get; set; }
+        public string Nombre { get; set; }
+        public string TipoDePerfume { get; set; }
+        public int Presentacion { get; set; }
+        public string Genero { get; set; }
+        public string Imagen { get; set; }
+        public decimal PrecioOriginal { get; set; }
+        public decimal PrecioConDescuento { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Total { get; set; }
+        public bool TienePromo { get; set; }
+    }
+}
