f10582b868ac25611225126f244803cb070c8a96
joseluisx10 <joseluis.quispe_91@hotmail.com>
2025-07-10 21:12:11 -0300

    Diseño y funcionamiento de Promocion

--- Eterea_Parfums_Web/Controllers/PromocionController.cs
+++ Eterea_Parfums_Web/Controllers/PromocionController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Eterea_Parfums_Web.Models;
 using Eterea_Parfums_Web.ViewModels;
 
 namespace Eterea_Parfums_Web.Controllers
 {
     public class PromocionController : Controller
     {
+
+        private etereaEntities7 db = new etereaEntities7();
         // GET: Promocion
         public ActionResult Index()
         {
