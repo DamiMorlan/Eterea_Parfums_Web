dc1ae75cc365c83eaaf7228a87b50d5f1afbeb36
AdrianBCampos <adrian.campos@davinci.edu.ar>
2025-07-21 20:26:53 -0300

    Conexion y ruta imagenes actualizada

--- Eterea_Parfums_Web/Controllers/ImagenController.cs
+++ Eterea_Parfums_Web/Controllers/ImagenController.cs
@@ -11,10 +11,10 @@ namespace Eterea_Parfums_Web.Controllers
     {
 
         /*Adri*/
-        //private string rutaBase = @"C:\Users\intersan\Desktop\TESIS_New\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
+        private string rutaBase = @"C:\Users\intersan\Desktop\TESIS_New\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
 
         /*Dami*/
-        private string rutaBase = @"C:\Users\damim\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
+        //private string rutaBase = @"C:\Users\damim\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
 
 
         public ActionResult Mostrar(string nombre)
