0b97bd0892b954cae31a1602d8cb4dda8b62fc90
MaxiKitagawa <maximiliano.kitagawa@davinci.edu.ar>
2025-07-29 22:54:52 -0300

    Fix datos de nueva bd

--- Eterea_Parfums_Web/Controllers/ImagenController.cs
+++ Eterea_Parfums_Web/Controllers/ImagenController.cs
@@ -14,7 +14,7 @@ namespace Eterea_Parfums_Web.Controllers
         //private string rutaBase = @"C:\Users\intersan\Desktop\TESIS_New\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
 
         /*Dami*/
-        private string rutaBase = @"C:\Users\damim\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
+        //private string rutaBase = @"C:\Users\damim\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
 
         /*Maxi*/
         private string rutaBase = @"C:\Users\Maxi\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
