7a853fdbb5339deb9d2ef3bef9fba0748c782c03
joseluisx10 <joseluis.quispe_91@hotmail.com>
2025-07-31 01:47:23 -0300

    validaciones cliente completo

--- Eterea_Parfums_Web/Controllers/ImagenController.cs
+++ Eterea_Parfums_Web/Controllers/ImagenController.cs
@@ -17,10 +17,10 @@ namespace Eterea_Parfums_Web.Controllers
         //private string rutaBase = @"C:\Users\damim\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
 
         /*Maxi*/
-        private string rutaBase = @"C:\Users\Maxi\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
+        //private string rutaBase = @"C:\Users\Maxi\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
 
         /*Jose Luis*/
-        //private string rutaBase = @"C:\Users\josel\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
+        private string rutaBase = @"C:\Users\josel\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
 
         public ActionResult Mostrar(string nombre)
         {
