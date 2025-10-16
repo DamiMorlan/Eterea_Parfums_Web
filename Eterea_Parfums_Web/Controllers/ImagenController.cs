4e7fa85df31a7d7528a5d67af1ee167d5e7cba24
DamiMorlan <damian.morlan@davinci.edu.ar>
2025-10-15 21:49:01 -0300

    webconfig

--- Eterea_Parfums_Web/Controllers/ImagenController.cs
+++ Eterea_Parfums_Web/Controllers/ImagenController.cs
@@ -12,10 +12,10 @@ namespace Eterea_Parfums_Web.Controllers
 
         /*Adri*/
         //private string rutaBase = @"C:\Users\intersan\Desktop\TESIS_New\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
-        //private string rutaBase = "http://eterea-001-site1.anytempurl.com/etereaparfums/Imagenes";
+        private string rutaBase = "http://eterea-001-site1.anytempurl.com/etereaparfums/Imagenes";
 
         /*Dami*/
-        private string rutaBase = @"C:\Users\damim\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
+        //private string rutaBase = @"C:\Users\damim\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
 
         /*Maxi*/
         //private string rutaBase = @"C:\Users\Maxi\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
