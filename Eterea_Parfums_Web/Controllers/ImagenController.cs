450c3c9cf4196b6a62cf65530841378e048ecabd
joseluisx10 <joseluis.quispe_91@hotmail.com>
2025-11-04 15:20:32 -0300

    inicio

--- Eterea_Parfums_Web/Controllers/ImagenController.cs
+++ Eterea_Parfums_Web/Controllers/ImagenController.cs
@@ -11,7 +11,7 @@ namespace Eterea_Parfums_Web.Controllers
     {
 
         /*Adri*/
-        private string rutaBase = @"C:\Users\intersan\Desktop\TESIS_New\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
+        //private string rutaBase = @"C:\Users\intersan\Desktop\TESIS_New\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
         //private string rutaBase = "http://eterea-001-site1.anytempurl.com/etereaparfums/Imagenes";
 
         /*Dami*/
@@ -21,7 +21,7 @@ namespace Eterea_Parfums_Web.Controllers
         //private string rutaBase = @"C:\Users\Maxi\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
 
         /*Jose Luis*/
-        //private string rutaBase = @"C:\Users\josel\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
+        private string rutaBase = @"C:\Users\josel\source\repos\Eterea_Parfums_Desktop\Eterea_Parfums_Desktop\Resources";
 
         public ActionResult Mostrar(string nombre)
         {
